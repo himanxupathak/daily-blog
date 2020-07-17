2b7ec1cd0230b46f16f025d789d06d9325d1e134
himanshu <pathakh035@gmail.com>
2020-07-17 11:13:55 +0545

    removeded production code

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 //jshint esversion:6
-if(process.env.NODE_ENV !== "production"){
-    require("dotenv").config();
-}
 const PORT = process.env.PORT || 5000;
 const express = require("express");
 const ejs = require("ejs");
